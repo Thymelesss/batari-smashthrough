e288eb46a70b8dc3c31062ca7aa323b4100f0752
Cooper Meers <68950613+Thymelesss@users.noreply.github.com>
2021-05-23 13:34:11 +0800

    you now get bullied
    oh and i added bricks who cares

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//welcome to the variable zone
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var x = canvas.width / 2;
@@ -10,9 +12,28 @@ var paddleWidth = 75;
 var paddleX = (canvas.width - paddleWidth) / 2;
 var rightPressed = false;
 var leftPressed = false;
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+var brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+var brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
 
+var ballColor =
+  "#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
 
-var ballColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+var bricks = [];
+for (var c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (var r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  }
+}
+
+//--------------------------------------------------------------------------------------
 
 function drawBall() {
   ctx.beginPath();
@@ -26,17 +47,53 @@ function drawBall() {
 
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
-    ballColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+    ballColor =
+      "#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
   }
   if (y + dy < ballRadius) {
     dy = -dy;
-    ballColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+    ballColor =
+      "#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
   } else if (y + dy > canvas.height - ballRadius) {
     if (x > paddleX && x < paddleX + paddleWidth) {
       dy = -dy;
-      ballColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+      ballColor =
+        "#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
     } else {
-      alert("GAME OVER");
+      var deathFunny = Math.floor(Math.random() * 10);
+      console.log(deathFunny);
+      switch (deathFunny) {
+        case 0:
+          alert("wow you suck");
+          break;
+        case 1:
+          alert("you're bad at this");
+          break;
+        case 2:
+          alert("uninstall game");
+          break;
+        case 3:
+          alert("you are garbage");
+          break;
+        case 4:
+          alert("yikes");
+          break;
+        case 5:
+          alert("sheeeeeeeeeeeeeesh");
+          break;
+        case 6:
+          alert("😐");
+          break;
+        case 7:
+          alert("you are noob man");
+          break;
+        case 8:
+          alert("sucks to suck");
+          break;
+        case 9:
+          alert("get a job");
+          break;
+      }
       document.location.reload();
       clearInterval(interval);
     }
@@ -51,10 +108,30 @@ function drawPaddle() {
   ctx.closePath();
 }
 
+function drawBricks() {
+  for (var c = 0; c < brickColumnCount; c++) {
+    for (var r = 0; r < brickRowCount; r++) {
+      if (bricks[c][r].status == 1) {
+        var brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+        var brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+  }
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBricks();
   drawBall();
   drawPaddle();
+  collisionDetection();
   x += dx;
   y += dy;
   if (rightPressed) {
@@ -86,6 +163,28 @@ function keyUpHandler(e) {
     rightPressed = false;
   } else if (e.key == "Left" || e.key == "ArrowLeft") {
     leftPressed = false;
+  }
+}
+
+function collisionDetection() {
+  for (var c = 0; c < brickColumnCount; c++) {
+    for (var r = 0; r < brickRowCount; r++) {
+      var b = bricks[c][r];
+      if (b.status == 1) {
+        if (
+          x > b.x &&
+          x < b.x + brickWidth &&
+          y > b.y &&
+          y < b.y + brickHeight
+        ) {
+          dy = -dy;
+          b.status = 0;
+          ballColor =
+            "#" +
+            (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
+        }
+      }
+    }
   }
 }
 
